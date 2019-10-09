63678ebae0787d0e9a008bf3d6234b26cd10ba40
chenweixuan <chenweixuan@tbs-info.com>
2019-10-09 17:42:08 +0800

    微小细节问题修改

---
+++ src/main/resources/static/assets/js/exe.js
@@ -0,0 +1,170 @@
+$(function () {
+    getQueryVariable("questionsStatus");
+    getData();
+
+
+    $('.select2').select2({
+        placeholder: '添加搜索标签',
+        maximumSelectionLength: 5,
+        allowClear: true
+    });
+});
+
+totalPage = 0;
+currentPageIndex=0;
+function getData(pageInfo={}) {
+    if(pageInfo.page==null || pageInfo.page == undefined || pageInfo.page=='') {
+        pageInfo.page=1;
+        currentPageIndex=1;
+    }
+
+
+    pageInfo.size=20;
+    $.ajax({
+        // async : false,    //表示请求是否异步处理
+        type : "get",    //请求类型
+        url : "/baseQuestions/getQuestionByPage",//请求的 URL地址
+        data: pageInfo,
+        dataType : "json",//返回的数据类型
+        success: function (data) {
+            console.log(data);  //在控制台打印服务器端返回的数据
+            if(data.code=='200') {
+                var page=data.body;
+                var questionList=data.body.records;
+                if(questionList.length != 0) {
+                    for(var i=0;i<questionList.length;i++) {
+                        console.log("heheh = " + questionList[i].questionType);
+                        $("#questionList").append(' <tr >\n' +
+                            '                                <td >'+questionList[i].id+'</td>\n' +
+                            '                                <td >'+questionList[i].content+'</td>\n' +
+                            '                                <td>'+questionTypeFormatter(questionList[i].questionType)+'</td>\n' +
+                            '                                <td>'+timeStamp2String(questionList[i].createdAt)+'</td>\n' +
+                            '                                <td >\n' +
+                            '<a href="javascript:edit('+questionList[i].id+')">查看</a>'+
+
+                            '                                </td>\n' +
+                            '<td></td>'+
+                            '                                <td><span class="badge badge-success">启用</span></td>\n' +
+                            '                            </tr>');
+                    }
+                }
+                totalPage=page.pages;
+                currentPageIndex=parseInt(currentPageIndex);
+                var showPage=totalPage;
+                $(".page-item").remove();
+                if(totalPage>3) {
+                    if(currentPageIndex-1<1) {
+                        var page1=currentPageIndex;
+                        var page2=currentPageIndex+1;
+                        var page3=currentPageIndex+2;
+                        $("#nextPage").before('<li class="page-item" data-id=' + page1 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page1 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page2 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page2 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page3 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page3 + '</a></li>');
+                    }else if(currentPageIndex+1>totalPage) {
+                        console.log("situation 2--"+currentPageIndex);
+                        console.log("totalpage--"+totalPage);
+                        var page1=parseInt(currentPageIndex);
+                        var page2=parseInt(currentPageIndex)-1;
+                        var page3=parseInt(currentPageIndex)-2;
+                        console.log("page----"+page1);
+                        console.log("page----"+page2);
+                        console.log("page----"+page3);
+                        $("#nextPage").before('<li class="page-item" data-id=' + page3 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page3 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page2 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page2 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page1 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page1 + '</a></li>');
+                    } else {
+                        var page1=parseInt(currentPageIndex)-1;
+                        var page2=parseInt(currentPageIndex);
+                        var page3=parseInt(currentPageIndex)+1;
+                        console.log("situation 3--"+currentPageIndex);
+                        console.log("page----"+page1);
+                        console.log("page----"+page2);
+                        console.log("page----"+page3);
+                        $("#nextPage").before('<li class="page-item" data-id=' + page1 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page1 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page2 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page2 + '</a></li>');
+                        $("#nextPage").before('<li class="page-item" data-id=' + page3 + '><a class="page-link" href="#" onclick="pageChange(this)">' + page3 + '</a></li>');
+                    }
+                } else {
+
+                    for (i = 0; i < totalPage; i++) {
+                        var current = i + 1;
+                        $("#nextPage").before('<li class="page-item" data-id=' + current + '><a class="page-link" href="#" onclick="pageChange(this)">' + current + '</a></li>');
+
+                    }
+                }
+
+            }
+            if(data.code=='500') {
+                alert(data.message);
+            }
+        },
+        error:function (data) {
+            alert(data.message);
+        }
+
+    });
+}
+
+function edit(id) {
+    var  pageInfo={};
+    pageInfo.page=1;
+    pageInfo.size=10;
+    pageInfo.id=id;
+    var url=encodeURI("math-exedit.html?questionId="+JSON.stringify(pageInfo));
+    window.location.href = url;
+
+
+}
+function add() {
+    window.location.href="math-exedit.html?newQuest=true";
+}
+function pageChange(target) {
+    currentPageIndex=$(target).text();
+    console.log(currentPageIndex);
+    var pageInfo={};
+    pageInfo.page=currentPageIndex;
+    getData(pageInfo);
+    $("#questionList").empty();
+
+}
+
+function nextPage() {
+    var nextPage=parseInt(currentPageIndex)+1;
+    console.log(nextPage);
+    if(nextPage>totalPage) {
+        return;
+    }
+    currentPageIndex=nextPage;
+    console.log(nextPage);
+    var pageInfo={};
+    pageInfo.page=nextPage;
+    getData(pageInfo);
+    $("#questionList").empty();
+
+}
+function lastPage() {
+
+    var lastPage=currentPageIndex-1;
+    console.log(lastPage);
+    if(lastPage<0) {
+        return;
+    }
+    currentPageIndex=lastPage;
+    console.log(lastPage);
+    var pageInfo={};
+    pageInfo.page=lastPage;
+    getData(pageInfo);
+    $("#questionList").empty();
+
+}
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
--- src/main/resources/static/assets/js/exedit.js
+++ src/main/resources/static/assets/js/exedit.js
@@ -1,15 +1,173 @@
 var editId;
 var editTags;
 $(function () {
+
+
+
+    getQuestionType();
+    if(getQueryVariable("newQuest")==false){
+        getData();
+    }else{
+        addNewQuestion()
+    }
+    CKEDITOR.replace( 'content' );
+    CKEDITOR.replace( 'answer' );
+    CKEDITOR.replace( 'analyis' );
     var data={}
     data.id=$('#id').val();
-    editId=data.id;
-    editTags=data;
-            showDepend(data)
+    if (editId==null||editId=="") {
+        editId=$('#id').val();
+        data.id=$('#id').val();
+        editTags = data;
 
+        showDepend(data)
+    }
     //得到全部能力点标签
-
 });
+
+tagsId=[];
+
+function getData(){
+
+    var questionInfo = window.location.href;
+    console.log('index of ='+questionInfo.indexOf("="));
+    if(questionInfo.indexOf("=")!= -1) {
+        var twodata = questionInfo.split("="); //截取 url中的“=”,获得“=”后面的参数
+        var data = decodeURI(twodata[1]); //decodeURI解码
+        var jsonData=JSON.parse(data);
+        console.log(jsonData.id);
+        dataToTable(jsonData);
+    }
+
+}
+
+function dataToTable(data) {
+    console.log("in data to table");
+    console.log(data);
+    $("#id").val(data.id);
+    $.ajax({
+        // async : false,    //表示请求是否异步处理
+        type : "get",    //请求类型
+        url : "/baseQuestions/getQuestionList",//请求的 URL地址
+        data: data,
+        dataType : "json",//返回的数据类型
+        success: function (data) {
+            console.log(data);  //在控制台打印服务器端返回的数据
+            if (data.code == '200') {
+                var questionList = data.body;
+                if (questionList.length != 0) {
+                    var questionInfo=null;
+                    for (var i = 0; i < questionList.length; i++) {
+                        questionInfo = questionList[i];
+                    }
+
+                    $("#content").text(questionInfo.content);
+                    $("#type").val(questionInfo.questionType);
+                    $("#answer").val(questionInfo.answer);
+                    $("#analyis").text(questionInfo.analysis);
+                    $("#years").val(questionInfo.questionDesignTime);
+                } else {
+                    alert("暂无数据");
+                }
+            }
+            if (data.code == '500') {
+                alert(data.message);
+            }
+        }
+    });
+
+}
+
+function getQuestionType() {
+    $.ajax({
+        // async : false,    //表示请求是否异步处理
+        type : "get",    //请求类型
+        url : "/baseQuestions/getQuestionType",//请求的 URL地址
+        dataType : "json",//返回的数据类型
+        success: function (data) {
+            console.log(data);  //在控制台打印服务器端返回的数据
+            if (data.code == '200') {
+                var types=data.body;
+                if(types.length != 0) {
+                    for (var i = 0; i < types.length; i++) {
+
+                        $("#type").append(' <option value='+types[i].questionTypeCode+'>'+types[i].questionTypeName+'</option>');
+                    }
+                }
+            }
+            if (data.code == '500') {
+                alert(data.message);
+            }
+        }
+    });
+
+
+}
+
+function addOrEdit(status) {
+
+    var questionInfo={};
+    questionInfo.id=$('#id').val();
+    questionInfo.content=CKEDITOR.instances.content.getData();
+    questionInfo.questionType=$("#type").val();
+    questionInfo.status=status;
+    if(questionInfo.questionType==-1){
+        alert("请修改题目类型");
+        return;
+    }
+    //questionInfo.optionList=
+    questionInfo.answer=CKEDITOR.instances.answer.getData();
+    questionInfo.analysis=CKEDITOR.instances.analyis.getData();
+    questionInfo.questionDesignTime=$("#years").val();
+    console.log(questionInfo);
+    var flag=vaild(questionInfo);
+    if(!flag) {
+        return;
+    }
+    //questionDesignTime
+    $.ajax({
+        // async : false,    //表示请求是否异步处理
+        type : "post",    //请求类型
+        url : "/baseQuestions/addOrUpdateQuestion",
+        contentType: 'application/json',//请求的 URL地址
+        data:JSON.stringify(questionInfo),
+        dataType : "json",//返回的数据类型
+        success: function (data) {
+            console.log(data);  //在控制台打印服务器端返回的数据
+            if (data.code == '200') {
+                alert("操作成功");
+                window.location.href="math-exe.html";
+            }
+            if (data.code == '500') {
+                alert(data.message);
+            }
+        }
+    });
+}
+
+
+function vaild(questionInfo) {
+    if(questionInfo.content==null || questionInfo.content=='') {
+        alert("请添加题干内容");
+        return false;
+    }
+    if(questionInfo.answer==null || questionInfo.answer=='') {
+        alert("请添加答案内容");
+        return false;
+    }
+    return true;
+}
+$('#years').datepicker({
+    startView: 2,
+    minViewMode: 2,
+    maxViewMode: 2,
+    language: "zh-CN",
+    format:"yyyy",
+    autoclose: true
+});
+
+
+
 //得到子标签列表
 function showDepend(parentdata) {
     $("#abilityTag").empty();
@@ -35,7 +193,7 @@ function showDepend(parentdata) {
                             );}
                         if (tagList[i].sonTags.tagType==2){
                             $("#intelligenceTag").append(
-                                '<span class="_tags" id="'+tagList[i].sonTags.id+'">'+tagList[i].parentTags.tagName+"-"+tagList[i].sonTags.tagName+' <i class="_tagsDelBtn">✖</i></span>'
+                                '<span class="_tags" id="'+tagList[i].sonTags.id+'">'+(tagList[i].parentTags!=null?tagList[i].parentTags.tagName+"-":"")+"-"+tagList[i].sonTags.tagName+' <i class="_tagsDelBtn">✖</i></span>'
                             );
                         }
                     }
@@ -50,66 +208,70 @@ function showDepend(parentdata) {
     });
     }
 
-    $.ajax({
-        async : false,    //表示请求是否异步处理
-        type : "get",    //请求类型
-        url : "/tags/getTagsByTypeAndParentId",//请求的 URL地址
-        data:{tagType:1,parentId:0} ,
-        dataType : "json",//返回的数据类型
-        success: function (data) {
-            console.log(data);  //在控制台打印服务器端返回的数据
-            if(data.code=='200') {
-                var tagList=data.body;
-                if(tagList.length != 0) {
-                    for(var i=0;i<tagList.length;i++) {
-                        if (tagList[i].tagType==1){
-                            $("#coreAbility").append(
-                                '<option value="'+tagList[i].id+'">'+tagList[i].tagName+'</option>'
-                            );}
-
-                    }
-                    $("#coreAbility").trigger('change');
-                }
-            }
-
-        },
-        error:function (data) {
-            alert("请刷新重试");
-        }
-
-    });
-    // 知识点
-    $.ajax({
-        async : false,    //表示请求是否异步处理
-        type : "get",    //请求类型
-        url : "/tags/getTagsByTypeAndParentId",//请求的 URL地址
-        data:{tagType:2,parentId:0} ,
-        dataType : "json",//返回的数据类型
-        success: function (data) {
-            console.log(data);  //在控制台打印服务器端返回的数据
-            if(data.code=='200') {
-                var tagList=data.body;
-                if(tagList.length != 0) {
-                    for(var i=0;i<tagList.length;i++) {
-                        if (tagList[i].tagType==2){
-                            $("#coreIntelligence").append(
-                                '<option value="'+tagList[i].id+'">'+tagList[i].tagName+'</option>'
-                            );}
-                    }
-                    $("#coreIntelligence").trigger('change');
-                }
-            }
-
-        },
-        error:function (data) {
-            alert("请刷新重试");
-        }
-
-    });
-
 }
+//能力
+$.ajax({
+    async : false,    //表示请求是否异步处理
+    type : "get",    //请求类型
+    url : "/tags/getTagsByTypeAndParentId",//请求的 URL地址
+    data:{tagType:1,parentId:0} ,
+    dataType : "json",//返回的数据类型
+    success: function (data) {
+        console.log(data);  //在控制台打印服务器端返回的数据
+        if(data.code=='200') {
+            var tagList=data.body;
+            if(tagList.length != 0) {
+                for(var i=0;i<tagList.length;i++) {
+                    if (tagList[i].tagType==1){
+                        $("#coreAbility").append(
+                            '<option value="'+tagList[i].id+'">'+tagList[i].tagName+'</option>'
+                        );}
+
+                }
+                $("#coreAbility").trigger('change');
+            }
+        }
+
+    },
+    error:function (data) {
+        alert("请刷新重试");
+    }
+
+});
+// 知识点
+$.ajax({
+    async : false,    //表示请求是否异步处理
+    type : "get",    //请求类型
+    url : "/tags/getTagsByTypeAndParentId",//请求的 URL地址
+    data:{tagType:2,parentId:0} ,
+    dataType : "json",//返回的数据类型
+    success: function (data) {
+        console.log(data);  //在控制台打印服务器端返回的数据
+        if(data.code=='200') {
+            var tagList=data.body;
+            if(tagList.length != 0) {
+                for(var i=0;i<tagList.length;i++) {
+                    if (tagList[i].tagType==2){
+                        $("#coreIntelligence").append(
+                            '<option value="'+tagList[i].id+'">'+tagList[i].tagName+'</option>'
+                        );}
+                }
+                $("#coreIntelligence").trigger('change');
+            }
+        }
+
+    },
+    error:function (data) {
+        alert("请刷新重试");
+    }
+
+});
+
 $("body").on('change','#coreAbility',function () {
     $("#otherAbility").empty();
+    $("#otherAbility").append(
+        '<option value="" ></option>>'
+    );
     var coreid=$(this).val();
     $.ajax({
         async : false,    //表示请求是否异步处理
@@ -201,7 +363,8 @@ $("#saveAblity").click(function () {
     appendDependcy( $("#otherAbility").val())
 })
 $("#saveIntelligence").click(function () {
-    appendDependcy( $("#otherIntelligence").val())
+    if($("#otherIntelligence").val()!=null&&$("#otherIntelligence").val()!=""){ appendDependcy( $("#otherIntelligence").val())}
+    else  if($("#coreIntelligence").val()!=null&&$("#coreIntelligence").val()!="") { appendDependcy( $("#coreIntelligence").val())}
 })
 function appendDependcy(tagid) {
     var GradesTags={};
@@ -269,6 +432,8 @@ function addNewQuestion() {
             console.log(data);  //在控制台打印服务器端返回的数据
             if (data.code == '200') {
                 var questionInfo=data.body;
+                editId=questionInfo.id;
+                editTags=questionInfo;
                 $("#id").val(questionInfo.id);
                 // $("#content").text(questionInfo.content);
                 $("#type").val(questionInfo.questionType);
